24933faf732b83a5182ddfd9a16ab68b0959070d
Suvarn Pillay <105659331+SuvarnPillayWits@users.noreply.github.com>
2024-08-27 11:51:52 +0200

    network looking good

---
+++ PHASE_2/README.md
@@ -0,0 +1,8 @@
+Okay, ground truth model seems to be set up
+
+1) Plot it better to make sure everything is connected properly
+2) Sample parameter data
+3) Forward sample
+5) Generate data using forward sampling
+6) Put data into correct format
+7) Generate learned model
--- PHASE_2/ground_truth.py
+++ PHASE_2/ground_truth.py
@@ -2,15 +2,13 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from pgmpy.models import BayesianNetwork, LinearGaussianBayesianNetwork
-from pgmpy.estimators import MaximumLikelihoodEstimator,ExpectationMaximization, BayesianEstimator
+from pgmpy.estimators import MaximumLikelihoodEstimator, ExpectationMaximization, BayesianEstimator
 from pgmpy.estimators import HillClimbSearch, BicScore, K2Score, BDsScore
 import matplotlib.pyplot as plt
 from pgmpy.inference import VariableElimination
 import networkx as nx
 
-
-def visualise(model):
-    layout = 'planar'
+def visualise(model, pos, projectile_color="skyblue", interceptor_color="lightcoral"):
     edges = model.edges()
 
     # Create a directed graph object
@@ -19,67 +17,96 @@ def visualise(model):
     # Add edges to the graph
     G.add_edges_from(edges)
 
-    # Choose the layout
-    if layout == 'spring':
-        pos = nx.spring_layout(G, k=0.5)
-    elif layout == 'circular':
-        pos = nx.circular_layout(G)
-    elif layout == 'shell':
-        pos = nx.shell_layout(G)
-    elif layout == 'spectral':
-        pos = nx.spectral_layout(G)
-    elif layout == 'kamada_kawai':
-        pos = nx.kamada_kawai_layout(G)
-    elif layout == 'planar':
-        pos = nx.planar_layout(G)
-    else:
-        pos = nx.spring_layout(G)  # Default
+    # Create a color map based on node type
+    color_map = []
+    for node in G.nodes():
+        if "P_" in node:
+            color_map.append(projectile_color)
+        elif "I_" in node:
+            color_map.append(interceptor_color)
+        else:
+            color_map.append("gray")  # Default color for any other nodes
 
-    # Plot the graph
+    # Plot the graph with custom positions and colors
     plt.figure(figsize=(20, 12))
-    nx.draw(G, pos, with_labels=True, node_size=1000, node_color="skyblue", font_size=12, font_weight="bold", arrowsize=10)
+    nx.draw(G, pos, with_labels=True, node_size=1000, node_color=color_map, font_size=8, font_weight="bold", arrowsize=10)
     plt.title("Bayesian Network Graph")
     plt.show()
 
-
-
-
-
-
-Projectile_edges = []
-
-Interceptor_nodes = []
-Projectile_nodes = []
-
-Interceptor_edges = []
+# Define positions manually
+pos = {}
 
 for i in range(6):
-    Projectile_nodes.append([f'Projectile_Final_Position_{i}', f'Projectile_Direction_{i}', f'Projectile_Velocity_{i}',
-                              f'Projectile_Initial_Position_{i}',  f'Projectile_Angle_{i}'])
-    
-    Interceptor_nodes.append([f'Interceptor_Final_Position_{i}', f'Interceptor_Direction_{i}', f'Interceptor_Velocity_{i}',
-                              f'Interceptor_Initial_Position_{i}', f'Interceptor_Angle_{i}'])
-    
-    Projectile_edges.append([(f'Projectile_Final_Position_{i}',f'Projectile_Direction_{i}'),(f'Projectile_Direction_{i}',f'Projectile_Initial_Position_{i}'),
-                    ( f'Projectile_Direction_{i}',f'Projectile_Angle_{i}'),(f'Projectile_Final_Position_{i}',f'Projectile_Velocity_{i}')])
-    
-    Interceptor_edges.append([(f'Interceptor_Final_Position_{i}',f'Interceptor_Direction_{i}'),(f'Interceptor_Direction_{i}',f'Interceptor_Initial_Position_{i}'),
-                    ( f'Interceptor_Direction_{i}',f'Interceptor_Angle_{i}'),(f'Interceptor_Final_Position_{i}',f'Interceptor_Velocity_{i}')])
-                              
-    
-for i in range(5):
-    Interceptor_edges.append([(f'Interceptor_Final_Position_{i}',f'Projectile_Final_Position_{i + 1}')])
 
+    if i % 2 == 0:
+        # Projectile nodes positions (lower row)
+        pos[f'P_FP{i}'] = ((i * 3)  , 0)
+        pos[f'P_D{i}'] = ((i * 3 - 1)  , -1)
+        pos[f'P_V{i}'] = ((i * 3 + 1)  , -1)
+        pos[f'P_IP{i}'] = ((i * 3 - 1)  , -2)
+        pos[f'P_A{i}'] = ((i * 3 + 1)  , -2)
+
+        # Interceptor nodes positions (upper row)
+        pos[f'I_FP{i}'] = (i * 3, 3)
+        pos[f'I_D{i}'] = (i * 3 - 1, 2)
+        pos[f'I_V{i}'] = (i * 3 + 1, 2)
+        pos[f'I_IP{i}'] = (i * 3 - 1, 1)
+        pos[f'I_A{i}'] = (i * 3 + 1, 1)
+    else:
+         # Projectile nodes positions (lower row)
+        pos[f'P_FP{i}'] = ((i * 3)  , (0) - 1)
+        pos[f'P_D{i}'] = ((i * 3 - 1)  ,( -1) - 1)
+        pos[f'P_V{i}'] = ((i * 3 + 1)  , (-1) - 1)
+        pos[f'P_IP{i}'] = ((i * 3 - 1)  , (-2) - 1)
+        pos[f'P_A{i}'] = ((i * 3 + 1)  , (-2) - 1)
+
+        # Interceptor nodes positions (upper row)
+        pos[f'I_FP{i}'] = (i * 3, (3) + 1)
+        pos[f'I_D{i}'] = (i * 3 - 1, (2) + 1)
+        pos[f'I_V{i}'] = (i * 3 + 1, (2) + 1)
+        pos[f'I_IP{i}'] = (i * 3 - 1, (1) + 1)
+        pos[f'I_A{i}'] = (i * 3 + 1, (1) + 1)
+
+
+# Create the Bayesian Network
 GT = BayesianNetwork()
+
 for i in range(6):
-    GT.add_nodes_from(Projectile_nodes[i])
-    GT.add_nodes_from(Interceptor_nodes[i])
-    GT.add_edges_from(Projectile_edges[i])
-    GT.add_edges_from(Interceptor_edges[i])
+    GT.add_nodes_from([
+        f'P_FP{i}', f'P_D{i}', f'P_V{i}',
+        f'P_IP{i}', f'P_A{i}',
+        f'I_FP{i}', f'I_D{i}', f'I_V{i}',
+        f'I_IP{i}', f'I_A{i}'
+    ])
+    
+    GT.add_edges_from([
+        (f'P_FP{i}', f'P_D{i}'),
+        (f'P_D{i}', f'P_IP{i}'),
+        (f'P_D{i}', f'P_A{i}'),
+        (f'P_FP{i}', f'P_V{i}'),
+        (f'I_FP{i}', f'I_D{i}'),
+        (f'I_D{i}', f'I_IP{i}'),
+        (f'I_D{i}', f'I_A{i}'),
+        (f'I_FP{i}', f'I_V{i}')
+    ])
 
-GT.add_edges_from(Interceptor_edges[6])  
+    if i < 5:
+        # Connect corresponding nodes between time slices
+        GT.add_edges_from([
+            (f'P_FP{i}', f'P_FP{i + 1}'),
+            (f'P_D{i}', f'P_D{i + 1}'),
+            (f'P_V{i}', f'P_V{i + 1}'),
+            (f'P_IP{i}', f'P_IP{i + 1}'),
+            (f'P_A{i}', f'P_A{i + 1}'),
+            (f'I_FP{i}', f'I_FP{i + 1}'),
+            (f'I_D{i}', f'I_D{i + 1}'),
+            (f'I_V{i}', f'I_V{i + 1}'),
+            (f'I_IP{i}', f'I_IP{i + 1}'),
+            (f'I_A{i}', f'I_A{i + 1}')
+        ])
+        
+        # Edge between interceptor and projectile across time slices
+        GT.add_edge(f'P_FP{i + 1}', f'I_FP{i}')
 
-# Call the function with your model and a different layout
-visualise(GT)
-
-# GT.add_edges_from(Interceptor_nodes)
+# Visualize the Bayesian Network with custom positions and colors
+visualise(GT, pos)
